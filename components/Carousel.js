0426d0fe4b3c121e18465b36385a21174e2c8996
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-04-19 15:00:25 -0600

    working on carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,51 +1,28 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'next/image';
 
-export default function ImageCarousel() {
-  return (
-    <Carousel fade>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="/public/assets/grantara.jpg"
-          alt="Picture of the author"
-          width={1000}
-          height={500}
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="/public/assets/grantara.jpg"
-          alt="Picture of the author"
-          width={1000}
-          height={500}
-        />
+export default function ImageCarousel(props) {
+  console.log(props.images);
+  console.log(props.images[0].props);
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          className="d-block w-100"
-          src="/public/assets/grantara.jpg"
-          alt="Picture of the author"
-          width={1000}
-          height={500}
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+  return (
+    //<p>Please work!</p>
+    <Carousel fade>
+      {props.images.map(image => {
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src={image.props.src}
+            alt="hello!"
+            width={1000}
+            height={500}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>;
+      })}
     </Carousel>
   );
 }
--- pages/canada.js
+++ pages/canada.js
@@ -1,3 +1,56 @@
-export default function Ireland() {
-  return <h1>Ireland Content!</h1>
+import Carousel from '@/components/Carousel';
+import Image from 'next/image';
+
+export default function Canada() {
+  let canadaImages = [<Image src="/UBC.jpg" alt=" UBC" width="64" height="64" />, <Image src="/vanDusen.jpg" alt=" vanDusen" width="64" height="64" />]
+  return (
+    <>
+      <div className="py-20 flex flex-col items-center justify-center dark:bg-navy">
+        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center text-navy">
+          <h1 className="text-6xl font-bold dark:text-gray-100">
+            Beautiful British Columbia
+          </h1>
+
+          <Carousel 
+          images={canadaImages}
+          />
+          {/* <p className="mt-3 text-2xl dark:text-gray-100">
+            Where I've been and where I hope to go.
+          </p> */}
+
+          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+            <a
+              href="https://nextjs.org/docs"
+              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Vancouver &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Delicious food, friendly people, and gorgeous scenery.
+              </p>
+            </a>
+
+            <a
+              href="https://nextjs.org/learn"
+              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Whistler &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Learn about Next.js in an interactive course with quizzes!
+              </p>
+            </a>
+
+            <a
+              href="https://nextjs.org/learn"
+              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Victoria &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Learn about Next.js in an interactive course with quizzes!
+              </p>
+            </a>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
