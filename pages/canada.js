8f6304f31d20b0ece1800e596474113fb1a7a2f9
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-04-19 19:52:16 -0600

    added constructor to photo carousel

--- pages/canada.js
+++ pages/canada.js
@@ -1,19 +1,40 @@
 import Carousel from '@/components/Carousel';
 import Image from 'next/image';
+import { UncontrolledCarousel, Row, Col } from 'reactstrap';
 
 export default function Canada() {
-  let canadaImages = [<Image src="/UBC.jpg" alt=" UBC" width="64" height="64" />, <Image src="/vanDusen.jpg" alt=" vanDusen" width="64" height="64" />]
+  function Photo(altText, caption, header) {
+    this.src = `/${altText}.jpg`;
+    this.altText = altText;
+    this.caption = caption;
+    this.header = header;
+    this.key = Math.random();
+  }
+  
+  let canadaImages = [];
+  function addPhoto (alt, caption, header){
+    canadaImages.push(new Photo(alt, caption, header));
+  }
+  addPhoto("UBC", "Here is a caption", "UBC Museum");
+  addPhoto("vanDusen", "caption2", "VanDusen Botanical Garden");
+  addPhoto("whistler", "caption2", "Whistler Blackcomb");
+
+  
+  console.log(canadaImages[0].src)
+  console.log(canadaImages)
   return (
     <>
-      <div className="py-20 flex flex-col items-center justify-center dark:bg-navy">
+      <div className="flex flex-col items-center justify-center dark:bg-navy">
         <main className="flex flex-col items-center justify-center flex-1 px-20 text-center text-navy">
-          <h1 className="text-6xl font-bold dark:text-gray-100">
+          <h1 className="text-6xl m-6 font-bold dark:text-gray-100">
             Beautiful British Columbia
           </h1>
-
-          <Carousel 
-          images={canadaImages}
-          />
+          <Row>
+        <Col md="8" className="mx-auto">
+          <UncontrolledCarousel items={canadaImages} />
+        </Col>
+      </Row>
+    
           {/* <p className="mt-3 text-2xl dark:text-gray-100">
             Where I've been and where I hope to go.
           </p> */}
