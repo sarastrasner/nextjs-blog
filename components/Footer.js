ecdeb28a888b2ba3901a43e6f001b059be4894d0
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-04-20 20:47:24 -0600

    cleaned up footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="flex bg-green items-center justify-center text-lg mb-0 w-full h-24">
-      <p className="text-navy dark:text-white font-medium">Meow! This is a footer!</p>
+    <footer className="flex bg-green items-center justify-center text-lg my-0 w-full h-24">
+      <p className="text-navy dark:text-white font-medium">
+        Meow! This is a footer!
+      </p>
     </footer>
   );
 }
