203c10eef530a807da6ca4dfe543b4728e4149b0
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-04-26 16:26:29 -0600

    added content

--- pages/[location]/[subLocation].js
+++ pages/[location]/[subLocation].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import locationData from '@/data/data.json';
 import Carousel from '@/components/Carousel';
-import Image from 'next/image'
+import Image from 'next/image';
 
 export function getStaticPaths() {
   return { paths: [], fallback: true };
@@ -34,7 +34,7 @@ export default function Location(props) {
           {currentSubPage.postTitle}
         </h1>
         <div class="grid lg:grid-cols-3 gap-4 sm:grid-cols-1">
-          <div className="bg-navy text-white p-4 text-left">
+          <div className="bg-navy dark:bg-green text-white p-4 text-left">
             <h2 className="m-0 text-center">
               {subLocation.charAt(0).toUpperCase() + subLocation.slice(1)} at a
               Glance
@@ -53,23 +53,22 @@ export default function Location(props) {
             </div>
           ) : null}
         </div>
-      </div>
+    
 
       <div className="p-9 dark:text-white">
         {currentSubPage.postList
           ? currentSubPage.postList.map((place, idx) => (
               <div className="justify-center py-3" key={idx}>
-                <h2 className="text-center">{place.title}</h2>
                 {place.image ? (
                   <Image
                     className="d-block w-100"
-                    src="/portree.jpg"
+                    src={`/${place.image}.jpg`}
                     alt={place.image}
                     width={750}
                     height={562}
                   />
                 ) : null}
-
+                <h2 className="text-center">{place.type}{place.title}</h2>
                 <p>{place.content}</p>
                 {place.tip ? (
                   <p>
@@ -81,6 +80,7 @@ export default function Location(props) {
             ))
           : null}
       </div>
+    </div>
     </div>
   );
 }
